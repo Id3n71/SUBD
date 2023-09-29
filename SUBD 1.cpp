2bb8865440b2e0c58cde62b212b45b4aa4b4ecff
Komarichev Goergii <71156640+Id3n71@users.noreply.github.com>
2023-09-29 09:37:07 +0300

    Update SUBD 1.cpp

--- SUBD 1.cpp
+++ SUBD 1.cpp
@@ -1,33 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <random>
-#include <cmath>
 
 using namespace std;
 
-int findMin(int* arr, int size) {
-    int min = arr[0];
-    for (int i = 1; i < size; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-    }
-    return min;
-}
+size_t sizeValidation();
+void fillArray(int* arr, const size_t size, const int minimumNum, const int maximumNum);
+void printArray(int* arr, const size_t size);
+int findMin(int* arr, const size_t size);
 
 int main() {
-
-    int size;
-    cout << "Enter size of array: ";
+    size_t size;
     try {
-        cin >> size;
-        if (size <= 0) {
-            throw out_of_range("Error,enter correct size");
-        }
+        size = sizeValidation();
     }
     catch (out_of_range& e) {
         cerr << e.what();
         return 1000;
     }
+    int* arr = new int[size];
 
     cout << "Enter minimum number:";
     int minimumNum;
@@ -36,31 +26,62 @@ int main() {
     cout << "Enter maximum number:";
     int maximumNum;
     cin >> maximumNum;
-
-    // заполнение массива случайными числами
-    random_device rd;
-    mt19937 gen(rd());
-
-    const uniform_int_distribution<int> uniformIntDistribution(minimumNum, maximumNum);
-
-    int* arr = new int[size]; // создание динамического массива
-    for (size_t i = 0; i < size; i++) {
-        arr[i] = uniformIntDistribution(gen);
-    }
-
-    // вывод массива
-    cout << "Your array: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
+    fillArray(arr, size, minimumNum, maximumNum);
+    printArray(arr, size);
+    
     // нахождение наименьшего числа в массиве
     int min = findMin(arr, size);
 
     cout << "Minimum of aray: " << min << endl;
 
-    delete[] arr; // освобождение памяти, выделенной для массива
-
+    if (arr!= nullptr)
+    {
+        delete[] arr;  // освобождение памяти, выделенной для массива
+            arr = nullptr;
+    }
     return 0;
+}
+
+int findMin(int* arr, const size_t size) {
+    int min = arr[0];
+    for (size_t i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+size_t sizeValidation()
+{
+    int size;
+        cout << "Enter size of array: ";
+        cin >> size;
+        if (size <= 0) {
+            throw out_of_range("Error,enter correct size");
+        }
+
+    return size;
+}
+
+void fillArray(int* arr, const size_t size, const int minimumNum, const int maximumNum)
+{ // заполнение массива случайными числами
+    random_device rd;
+    mt19937 gen(rd());
+
+    const uniform_int_distribution<int> uniformIntDistribution(minimumNum, maximumNum);
+
+    for (size_t i = 0; i < size; i++) {
+        arr[i] = uniformIntDistribution(gen);
+    }
+}
+
+void printArray(int* arr, const size_t size)
+{// вывод массива
+    cout << "Your array: ";
+    for (size_t i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
 }
